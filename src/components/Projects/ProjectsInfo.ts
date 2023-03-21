428c477a113a42f5e4d831bcacfa1832e2aa7a8a
Joshowens16 <joshowens16@gmail.com>
2023-03-21 12:09:40 -0700

    updated portfolio links

--- src/components/Projects/ProjectsInfo.ts
+++ src/components/Projects/ProjectsInfo.ts
@@ -13,14 +13,14 @@ const projects: ProjectType[] = [
       "Fast-paced and easy to learn, simply click the 'create room' button, and invite your friends by sending them the URL in your web browser. Everyone should now enter their usernames, assign themselves teams and roles. The host can now setup the game board- and you're ready to play CODENAMES!",
     image: '/CODENAMES.jpg',
     github: 'https://github.com/2208-Capstone-Team-2/codenames',
-    live: 'https://codenames-0nt7.onrender.com/',
+    live: 'https://codenames-production.up.railway.app/',
   },
   {
     title: 'Comic Overflow',
     description: 'Comic Overflow is a e-commerce web application.',
     image: '/comicoverflowimg.png',
     github: 'https://github.com/2208-GS-Team-1/Comic-Overflow',
-    live: 'https://comic-overflow.onrender.com/',
+    live: 'https://comic-overflow-production.up.railway.app/',
   },
   {
     title: 'Personal Site',
